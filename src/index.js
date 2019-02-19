4eae790fdcdd4e2389fb48edf6e6ad72f850f3bf
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-19 18:00:27 +0300

    25/100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = 0;
+  let power = 2,
+    exponent = 0;
+  while (power <= base) {
+    let baseCopy = base; //копия base
+    //находим простой множитель
+    if (baseCopy % power != 0) {
+      power++;
+      continue;
+    }
+    while (baseCopy % power == 0) {
+      baseCopy /= power;
+      exponent++;
+    }
+    //
+    let tempExponent = 1;
+    let tempCount = 0;
+    //Находим количество нулей на конце
+    while (power ** tempExponent <= number) {
+      tempCount += Math.floor(number / (power ** tempExponent));
+      tempExponent++;
+    }
+    tempCount = tempCount / exponent;
+    if (count == 0) {
+      count = tempCount;
+    } else if (count > tempCount) {
+      count = tempCount;
+    }
+    power++;
+    exponent = 0;
+  }
+  return count;
 }
